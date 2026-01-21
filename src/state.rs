86861057d1c53263ebc30f4cff323adfac934ec3
Elijah Zimmerman <elijahz0409@gmail.com>
2026-01-21 15:52:47 -0500

    Implement tikz builder for State

--- src/state.rs
+++ src/state.rs
@@ -44,4 +44,55 @@ impl State {
             accepting: flags.is_accepting(),
         })
     }
+
+    pub fn build(&self, assume_math: bool) -> String {
+        let initial: &str = if self.initial.is_true() {
+            ", initial"
+        } else {
+            ""
+        };
+        let accepting: &str = if self.accepting {
+            ", accepting"
+        } else {
+            ""
+        };
+        let mut positional_args: Vec<String> = Vec::new();
+        self.positional_args.clone_into(&mut positional_args);
+        let positional_args: String = positional_args.into_iter().fold(String::new(), |acc, e| {
+            format!("{acc}, {e}")
+        });
+        let var = &self.var;
+        let name: String = if let Some(s) = &self.name {
+            if assume_math {
+                format!("${s}$")
+            } else {
+                s.to_owned()
+            }
+        } else {
+            "".to_string()
+        };
+        format!("\\node[state{initial}{accepting}{positional_args}] ({var}) {{{name}}};")
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_create_state() {
+        let state = State::from("s3", Some("$S_3$"), vec!["right of=s2"], StateFlags::Accepting).unwrap();
+        assert_eq!(state, State {
+            var: "s3".to_string(),
+            name: Some("$S_3$".to_string()),
+            positional_args: vec!["right of=s2".to_string()],
+            initial: UniqueBool::new(),
+            accepting: true
+        })
+    }
+
+    #[test]
+    fn test_build_state() {
+
+    }
 }
